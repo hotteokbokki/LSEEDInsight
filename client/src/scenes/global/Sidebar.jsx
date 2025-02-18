8c1f36bfdbd008f9e3001d5f2f03a45e72ef3286
dycanee <kendycane13@gmail.com>
2025-02-18 14:16:44 +0800

    mentor can access LSEED calendar

--- client/src/scenes/global/Sidebar.jsx
+++ client/src/scenes/global/Sidebar.jsx
@@ -108,7 +108,7 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  {user.name || "User"}
+                  {user.firstname || "User"}
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
                   {user.role || "LSEED User"}
